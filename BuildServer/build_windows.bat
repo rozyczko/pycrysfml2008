0b652ffe9f17c57a0e3bea1343553e2506dbc40f
Elisa Rebolini <rebolini@ill.fr>
2023-03-29 16:00:51 +0200

    add libpython"

--- BuildServer/build_windows.bat
+++ BuildServer/build_windows.bat
@@ -1,6 +1,6 @@
 set LIB="C:\\Program Files (x86)\\IntelSWTools\\compilers_and_libraries_2020.2.254\\windows\\compiler\\lib\\intel64_win";%LIB%
 set PATH="C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x64";%PATH%
-
+set LIBPYTHON="C:\\Projects\\mdanse\\resources\\dependencies\\win-amd64\\Python3\\libs\\python36.lib"
 md %CI_PROJECT_DIR%\\CFML08_Install
 set CRYSFML08_INSTALL=%CI_PROJECT_DIR%\\CFML08_Install
 git clone https://code.ill.fr/rodriguez-carvajal/CrysFML2008 crysfml2008
