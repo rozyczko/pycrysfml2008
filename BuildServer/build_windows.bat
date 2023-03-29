14542c5d7b541baf3db43b3241a2606ad9c67432
Elisa Rebolini <rebolini@ill.fr>
2023-03-29 14:56:52 +0200

    update bat script

--- BuildServer/build_windows.bat
+++ BuildServer/build_windows.bat
@@ -1,3 +1,6 @@
+set LIB="C:\\Program Files (x86)\\IntelSWTools\\compilers_and_libraries_2020.2.254\\windows\\compiler\\lib\\intel64_win";%LIB%
+set PATH="C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x64";%PATH%
+
 md %CI_PROJECT_DIR%\\CFML08_Install
 set CRYSFML08_INSTALL=%CI_PROJECT_DIR%\\CFML08_Install
 git clone https://code.ill.fr/rodriguez-carvajal/CrysFML2008 crysfml2008
@@ -5,7 +8,6 @@ cd crysfml2008 && md build && cd build && cmake -G "NMake Makefiles" -D CMAKE_BU
 cd %CRYSFML08_INSTALL%\\libC
 move crysfml.lib libcrysfml08.a
 cd %CI_PROJECT_DIR%
-set USE_PERL="YES"
 
 call %CI_PROJECT_DIR%\\scripts\\windows\\make_ifort_pycrysfml08.bat
 set STATUS=%ERRORLEVEL%
