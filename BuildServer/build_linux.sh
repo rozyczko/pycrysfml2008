0e0a4b3fd8a8fb698f51d63242b5c26a74d710e8
Elisa Rebolini <rebolini@ill.fr>
2024-02-27 11:47:40 +0100

    update environnement variables

--- BuildServer/build_linux.sh
+++ BuildServer/build_linux.sh
@@ -1,18 +1,22 @@
 echo "Building PyCrysFML08 with ifort"
 
 export CI_PROJECT_DIR=`pwd`
-export CRYSFML08=`pwd`/crysfml08
-export CRYSFML08_INSTALL=${CI_PROJECT_DIR}/CFML08_Install
+export CRYSFML08_REPO=`pwd`/crysfml08_repo
+export CRYSFML08_DIST=`pwd`/crysfml08_dist
 
-git clone https://code.ill.fr/rodriguez-carvajal/CrysFML2008 crysfml08
-cd crysfml08
+export INSTALLATION_DIR=`pwd`/pycrysfml08_dist
+export CRYSFML08_INCLUDE_DIR=${CRYSFML08_DIST}/include
+export CRYSFML08_LIB_DIR=${CRYSFML08_DIST}/lib
+
+git clone https://code.ill.fr/rodriguez-carvajal/CrysFML2008 ${CRYSFML08_REPO}
+cd ${CRYSFML08_REPO}
 mkdir build
 cd build
-cmake -D ARCH32=OFF -D PYTHON_API=OFF -D CMAKE_BUILD_TYPE=Debug -D CMAKE_Fortran_COMPILER=ifort -D CMAKE_INSTALL_PREFIX=${CRYSFML08_INSTALL} ..
+cmake -D ARCH32=OFF -D PYTHON_API=OFF -D CMAKE_BUILD_TYPE=Debug -D CMAKE_Fortran_COMPILER=ifort -D CMAKE_INSTALL_PREFIX=${CRYSFML08_DIST} ..
 cmake --build .
 make install
 
-cd $CRYSFML08_INSTALL/lib
+cd $CRYSFML08_DIST/lib
 ln -s libcrysfml.a libcrysfml08.a
 
 cd $CI_PROJECT_DIR/scripts/linux
