25938a247d01a19cb8f56048954421ea16a4f092
Elisa Rebolini <rebolini@ill.fr>
2024-02-28 17:06:32 +0100

    update lib name

--- BuildServer/build_windows.bat
+++ BuildServer/build_windows.bat
@@ -5,7 +5,7 @@ set INSTALLATION_DIR=%CI_PROJECT_DIR%\\pycrysfml08_dist
 git clone https://code.ill.fr/rodriguez-carvajal/CrysFML2008 crysfml2008_repo
 cd crysfml2008_repo && md build && cd build && cmake -G "NMake Makefiles" -D CMAKE_BUILD_TYPE=Debug -D CMAKE_INSTALL_PREFIX=%CRYSFML08_INSTALL% .. && cmake --build . && cmake --install . 
 cd %CRYSFML08_INSTALL%\\lib
-move crysfml.lib libcrysfml08.a
+move crysfml08.lib libcrysfml08.a
 cd %CI_PROJECT_DIR%
 
 cd %CI_PROJECT_DIR%\\scripts\\windows
